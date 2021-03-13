79290cbc2e4504cbdfe6f8b576ba8fc08c29858b
babusharif875 <payelbegum96@gmail.com>
2021-03-13 20:43:30 +0600

    Speling?

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
-var name = prompt("Enter Your name")
+var name = prompt("Enter a digit")
 
-document.write(name + " is my name")
-console.log("Thanks")
+switch (name) {
+
+case "0":
+    document.write("Zero");
+             break;
+    case "1":
+        document.write("One");
+                break;
+        case "2":
+            document.write("Two");
+                     break;
+            case "3":
+                document.write("Three");
+                         break;
+                case "4":
+                    document.write("Four");
+                            break;
+                    case "5":
+                        document.write("Five");
+                                break;
+                        case "6":
+                            document.write("Six");
+                                    break;
+                            case "7":
+                                document.write("Seven");
+                                        break;
+                                case "8":
+                                    document.write("Eight");
+                                            break;
+                                    case "9":
+                                        document.write("Nine");
+                                        break;
+                                        default:
+                                                 document.write("not a digit");                                                                                               
+
+
+
+
+
+}
 
 
 
